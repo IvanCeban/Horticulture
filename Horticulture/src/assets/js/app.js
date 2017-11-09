4683eb950b9ba6e26adb8f081daf904dc50d1d87
IC <himac!>
2017-11-09 13:13:06 +0200

    services page modified

--- Horticulture/src/assets/js/app.js
+++ Horticulture/src/assets/js/app.js
@@ -253,26 +253,6 @@ function MyOutdatedBrowser() {
     })
 }
 
-// //event listener: DOM ready
-// function addLoadEvent(func) {
-//     var oldonload = window.onload;
-//     if (typeof window.onload != 'function') {
-//         window.onload = func;
-//     } else {
-//         window.onload = function() {
-//             if (oldonload) {
-//                 oldonload();
-//             }
-//             func();
-//         }
-//     }
-// }
-// //call plugin function after DOM ready
-// addLoadEvent(function(){
-//     outdatedBrowser({
-//         bgColor: '#f25648',
-//         color: '#ffffff',
-//         lowerThan: 'transform',
-//         languagePath: ''
-//     })
-// });
+$('ul#services_tabs li.tabs-title a').on('click', function () {
+   $('.services_info_block').hide();
+});
